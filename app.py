9a22ed5375ed40213f6e7ca40234108cf91fc702
ddarkr <ddark.kr@gmail.com>
2020-11-04 14:25:25 +0900

    feat: return json with jsonify

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return jsonify(
+        message='Hello, world!',
+        version=1.0
+    )
